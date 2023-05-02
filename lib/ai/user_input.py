e67586c26893831e462f3c0d3a0913817912d328
Grant Carthew <grant@carthew.net>
2023-05-03 08:19:47 +1000

    feat: added other prompt features

--- lib/ai/user_input.py
+++ lib/ai/user_input.py
@@ -3,6 +3,7 @@ from lib.definitions import AI_HISTORY_PATH
 from rich import print as rprint
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.styles import Style
 import sys
 
@@ -15,7 +16,13 @@ def initial_message():
         rprint('[cyan]Type a file path to send the file contents[/]')
         rprint('[cyan]Multiline input enabled[/]')
         print_line()
-        rprint('[white]ESCAPE then ENTER to submit:[/]')
-        return session.prompt('> ', multiline=True, prompt_continuation='> ')
+        bottom_toolbar = 'ESCAPE then ENTER to submit | CTRL + SHIFT + V to paste'
+        return session.prompt(
+            '> ',
+            multiline=True,
+            prompt_continuation='> ',
+            mouse_support=True,
+            bottom_toolbar=bottom_toolbar,
+            auto_suggest=AutoSuggestFromHistory())
     except KeyboardInterrupt:
         sys.exit(0)
